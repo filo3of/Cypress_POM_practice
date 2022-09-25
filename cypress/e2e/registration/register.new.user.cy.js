cf3e1f711e0ef3661eddf33fa982a78eeb9d870c
Marko Bogicevic <marko@millions.co>
2022-09-25 20:45:01 +0200

    Test CircleCI pipeline

--- cypress/e2e/registration/register.new.user.cy.js
+++ cypress/e2e/registration/register.new.user.cy.js
@@ -1,61 +1,61 @@
-import Header from "../../POM/components/header.component";
-import Authentication from "../../POM/pages/authentication.page";
-import Registration from "../../POM/pages/registration.page";
-import HomePage from "../../POM/pages/home.page";
-import Data from "../../support/data";
+// import Header from "../../POM/components/header.component";
+// import Authentication from "../../POM/pages/authentication.page";
+// import Registration from "../../POM/pages/registration.page";
+// import HomePage from "../../POM/pages/home.page";
+// import Data from "../../support/data";
 
-describe("Registration", () => {
-  it("Register new female user", () => {
-    HomePage.open_HomePage(Data.device);
+// describe("Registration", () => {
+//   it("Register new female user", () => {
+//     HomePage.open_HomePage(Data.device);
 
-    Header.click_SignIn_button();
+//     Header.click_SignIn_button();
 
-    Authentication.enter_NewEmail(
-      Data.woman_email
-    ).click_CreateAccount_button();
+//     Authentication.enter_NewEmail(
+//       Data.woman_email
+//     ).click_CreateAccount_button();
 
-    Registration.select_gender(Data.woman)
-      .enter_FirstName(Data.first_Female_Name)
-      .enter_LastName(Data.last_Female_Name)
-      .verify_EnteredEmailAddress(Data.woman_email)
-      .enter_Password(Data.password)
-      .verify_Address_FirstName(Data.first_Female_Name)
-      .verify_Address_LastName(Data.last_Female_Name)
-      .enter_Address(Data.address)
-      .enter_City(Data.city)
-      .selectState(Data.state)
-      .enter_Postcode(Data.postcode)
-      .select_Country("United States")
-      .enter_MobilePhone(Data.phoneNumber)
-      .click_OnSubmit_Button()
-      .verify_user_is_registered();
+//     Registration.select_gender(Data.woman)
+//       .enter_FirstName(Data.first_Female_Name)
+//       .enter_LastName(Data.last_Female_Name)
+//       .verify_EnteredEmailAddress(Data.woman_email)
+//       .enter_Password(Data.password)
+//       .verify_Address_FirstName(Data.first_Female_Name)
+//       .verify_Address_LastName(Data.last_Female_Name)
+//       .enter_Address(Data.address)
+//       .enter_City(Data.city)
+//       .selectState(Data.state)
+//       .enter_Postcode(Data.postcode)
+//       .select_Country("United States")
+//       .enter_MobilePhone(Data.phoneNumber)
+//       .click_OnSubmit_Button()
+//       .verify_user_is_registered();
 
-    Data.save_email(Data.woman_email);
-  });
+//     Data.save_email(Data.woman_email);
+//   });
 
-  it("Register new male user", () => {
-    HomePage.open_HomePage(Data.device);
+//   it("Register new male user", () => {
+//     HomePage.open_HomePage(Data.device);
 
-    Header.click_SignIn_button();
+//     Header.click_SignIn_button();
 
-    Authentication.enter_NewEmail(Data.man_email).click_CreateAccount_button();
+//     Authentication.enter_NewEmail(Data.man_email).click_CreateAccount_button();
 
-    Registration.select_gender(Data.man)
-      .enter_FirstName(Data.first_Male_Name)
-      .enter_LastName(Data.last_Male_Name)
-      .verify_EnteredEmailAddress(Data.man_email)
-      .enter_Password(Data.password)
-      .verify_Address_FirstName(Data.first_Male_Name)
-      .verify_Address_LastName(Data.last_Male_Name)
-      .enter_Address(Data.address)
-      .enter_City(Data.city)
-      .selectState(Data.state)
-      .enter_Postcode(Data.postcode)
-      .select_Country("United States")
-      .enter_MobilePhone(Data.phoneNumber)
-      .click_OnSubmit_Button()
-      .verify_user_is_registered();
+//     Registration.select_gender(Data.man)
+//       .enter_FirstName(Data.first_Male_Name)
+//       .enter_LastName(Data.last_Male_Name)
+//       .verify_EnteredEmailAddress(Data.man_email)
+//       .enter_Password(Data.password)
+//       .verify_Address_FirstName(Data.first_Male_Name)
+//       .verify_Address_LastName(Data.last_Male_Name)
+//       .enter_Address(Data.address)
+//       .enter_City(Data.city)
+//       .selectState(Data.state)
+//       .enter_Postcode(Data.postcode)
+//       .select_Country("United States")
+//       .enter_MobilePhone(Data.phoneNumber)
+//       .click_OnSubmit_Button()
+//       .verify_user_is_registered();
 
-    Data.save_email(Data.man_email);
-  });
-});
+//     Data.save_email(Data.man_email);
+//   });
+// });
